8758898928ed39cde0d2534ba1665db13d2bf7d1
Conlectus <sam.doiron96@gmail.com>
2013-06-30 20:10:22 -0300

    Updated C helper files

--- fish.c
+++ fish.c
@@ -1,33 +1,84 @@
 #include "fish.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+/* Lists */
 
-/* Printing Functions */
-void pint(int a) {
-    printf("%d\n", a);
+// ListInt 
+struct ListInt {
+    int *list;
+    int space;
+    int spaceBuffer;
+    int next;
+};
+
+ListInt *ListIntCreate() {
+    ListInt *list = malloc(sizeof(ListInt));
+    list->space = 100;
+    list->spaceBuffer = list->space;
+    list->next = 0;
+    list->list = malloc(sizeof(int) * list->space);
+    return list;
 }
 
-void pstring(char *a) {
-    printf("%s\n", a);
+void addInt(ListInt *self, int a) {
+    if (self->next > self->space) {
+        // Too many items! We need to expand and copy
+        int *newList = malloc((sizeof(int) * self->next) \
+                + self->spaceBuffer);
+
+        for (int i = 0; i < self->next; i++) {
+            newList[i] = self->list[i];
+        }
+        
+        self->list = newList;
+    }
+
+    self->list[(self->next)++] = a;
 }
 
-void pdouble(double a) {
-    printf("%f\n", a);
+
+
+// ListString
+struct ListString {
+    char **list;
+    int space;
+    int spaceBuffer;
+    int next;
+};
+
+ListString *ListStringCreate() {
+    ListString *list = malloc(sizeof(ListString));
+    list->space = 100;
+    list->spaceBuffer = list->space;
+    list->next = 0;
+    list->list = malloc(list->space);
+    return list;
 }
 
-void pfloat(float a) {
-    printf("%s\n", a);
+void addString(ListString *self, char *a) {
+    if (self->next > self->space) {
+        // Too many items! We need to expand and copy
+        char **newList = malloc(self->next + self->spaceBuffer);
+
+        for (int i = 0; i < self->next; i++) {
+            newList[i] = self->list[i];
+        }
+        
+        self->list = newList;
+    }
+
+    self->list[(self->next)++] = a;
 }
 
 /* Conversion Functions */
-int str_int(char *a) {
+int str_to_int(char *a) {
     return atoi(a);
 }
 
-char *int_str(int a) {
-    char str[15];
+char *int_to_str(int a) {
+    char *str = malloc(15);
     sprintf(str, "%d", a);
     return str;
 }
-
 
--- fish.h
+++ fish.h
@@ -1,1 +1,9 @@
-void pint(int);
+struct ListInt;
+typedef struct ListInt ListInt;
+void addInt(ListInt*, int);
+ListInt *ListIntCreate();
+
+struct ListString;
+typedef struct ListString ListString;
+void addString(ListString*, char*);
+ListString *ListStringCreate();
